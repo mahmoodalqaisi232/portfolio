25296b02a0789037da2eb5a50f3bca9105957a99
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2022-01-28 00:35:45 +0100

    meta Tags

--- src/App.js
+++ src/App.js
@@ -11,23 +11,10 @@ import "./App.css";
 class App extends React.Component {
   scrolling(){window.onscroll=function(){window.scrollY>=1500?(document.getElementById("topBox").style.display="block"):(document.getElementById("topBox").style.display="none")}}
   backToTop(){window.scrollTo({top:0,left:0,})}
-  render() {
-    this.scrolling();
+  render() {this.scrolling();
     return (
       <>
       <div id="entrance"><span>Moe.Qaisi</span></div>
-        <div id="topBox">
-          <FontAwesomeIcon
-            icon={faChevronUp}
-            size="2x"
-            color="bisque"
-            className="arrow"
-            onClick={this.backToTop}/></div>
-        <Nav />
-        <Hero />
-        <Tech />
-        <Projects />
-        <About />
-        <Contact />
-      </>);}}
+        <div id="topBox"><FontAwesomeIcon icon={faChevronUp} size="2x" color="bisque" className="arrow" onClick={this.backToTop}/></div>
+        <Nav /><Hero /><Tech /><Projects /><About /><Contact /></>);}}
 export default App
